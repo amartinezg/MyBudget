006b71e43d54cbccf4e1e6d47e307a6979878a42
Andres Martinez <amartinez0000@gmail.com>
2015-12-25 16:45:25 -0500

    Annotate gem added

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id            :integer          not null, primary key
+#  name          :string
+#  type          :string
+#  balance_cents :integer
+#  currency      :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Account < ActiveRecord::Base
   monetize :balance_cents, presence: true
   has_many :movements
--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: movements
+#
+#  id           :integer          not null, primary key
+#  category     :string
+#  sub_category :string
+#  notes        :string
+#  amount_cents :integer
+#  type         :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  period       :date
+#  account_id   :string           not null
+#  date         :date
+#
+
 class Budget < Movement 
   validates :period, presence: true
   validates_numericality_of :amount_cents, greater_than: 0
