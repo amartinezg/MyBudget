642b7bb4423236f2edb6692c8a78e9af068929e0
Andres Martinez <amartinez0000@gmail.com>
2015-12-29 00:08:33 -0500

    Shoulda matcher configure fixed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -98,6 +98,8 @@ Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     require "active_model"
-    with.library :active_model
+    require "action_controller"
+    require "active_record"
+    with.library :rails
   end
 end
