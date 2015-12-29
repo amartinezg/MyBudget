c7da8b9ac24086352c129b516ab81276244c6b23
Andres Martinez <amartinez0000@gmail.com>
2015-12-29 00:43:11 -0500

    Seeds added for accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Account.create(name: "Dosh", type: "cash", balance: Money.new(5560000))
+Account.create(name: "Mastercard credit card", type: "credit", balance: Money.new(-60000000))
+Account.create(name: "US MasterCard credit card", type: "credit", :balance_currency => "USD", balance: Money.new(-20000, "USD"))
+Account.create(name: "Savings Payroll", type: "savings", balance: Money.new(120000000))
