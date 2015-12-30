80c856dafee40944de50a839bb424570947fae63
Andres Martinez <amartinez0000@gmail.com>
2015-12-30 13:50:32 -0500

    Annotate updated for spec

--- spec/factories/movements.rb
+++ spec/factories/movements.rb
@@ -2,17 +2,18 @@
 #
 # Table name: movements
 #
-#  id           :integer          not null, primary key
-#  category     :string
-#  sub_category :string
-#  notes        :string
-#  amount_cents :integer
-#  type         :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  period       :date
-#  account_id   :string           not null
-#  date         :date
+#  id              :integer          not null, primary key
+#  category        :string
+#  sub_category    :string
+#  notes           :string
+#  type            :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  period          :date
+#  account_id      :string
+#  date            :date
+#  amount_cents    :integer          default(0), not null
+#  amount_currency :string           default("COP"), not null
 #
 
 FactoryGirl.define do
