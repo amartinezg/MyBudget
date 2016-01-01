d478fc56b40cfb3c46cf2e7cab8ec1f45fdcb742
Andres Martinez <amartinez0000@gmail.com>
2016-01-01 16:40:04 -0500

    Migration fixed

--- db/migrate/20151226135119_monetize_account.rb
+++ db/migrate/20151226135119_monetize_account.rb
@@ -1,5 +1,6 @@
 class MonetizeAccount < ActiveRecord::Migration
   def change
+    remove_column :accounts, :balance_cents if column_exists?(:accounts, :balance_cents)
     add_monetize  :accounts, :balance, amount: { null: false, default: 0, postfix: '_cents' }
   end
 end
