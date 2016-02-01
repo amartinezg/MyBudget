2d850acdd21b4cdfa5f09d0df4c3b28770d917fb
Andres Martinez <amartinez0000@gmail.com>
2016-01-31 21:27:00 -0500

    Load movements from XML

--- app/models/account.rb
+++ app/models/account.rb
@@ -20,9 +20,33 @@ class Account < ActiveRecord::Base
   validates_inclusion_of :type, in: %w(savings credit cash)
   validates_uniqueness_of :name, scope: [:type, :balance_currency], message: "Account already exists"
 
+  XML_ATTRS = {date: "FECHA", category: "CATEGORIA", sub_category: "SUB_CATEGORIA", notes: "DESCRIPCIÓN"}
+
   ["increment", "decrement"].each do |action|
     define_method("#{action}_balance") do |amount|
       self.send("#{action}!".to_sym, :balance_cents, amount.cents)
     end
+  end
+
+  def load_movements_from_xml(path)
+    load_xml(path)
+    @xml.xpath("//Movimiento").map do |movement|
+      mov = create_instance(movement.xpath("VALOR"))
+      XML_ATTRS.each do |key, value|
+        mov.send("#{key}=".to_sym, movement.xpath(value).text)
+      end
+      mov.reconcile!
+    end
+  end
+
+  private
+  def load_xml(path)
+    @xml = File.open(path) { |f| Nokogiri::XML(f) }
+  end
+
+  def create_instance(amount)
+    amount = Money.new(amount.text.gsub(/[.,]/,""), balance_currency)
+    attributes = {amount: amount, account: self}
+    amount > 0 ? Income.new(attributes) : Expense.new(attributes)
   end
 end
