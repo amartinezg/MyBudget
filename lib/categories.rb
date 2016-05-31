7e4579de9da9be2a934a4fd1a51c38aeaaab428a
Andres Martinez <amartinez0000@gmail.com>
2016-05-31 19:24:54 -0300

    Categories processor validations added

--- lib/categories.rb
+++ lib/categories.rb
@@ -1,10 +1,23 @@
 module Categories
+  class InvalidCategoryError < StandardError; end
+  class InvalidSubCategoryError < StandardError; end
+
   def categories
     load_categories.keys
   end
 
+  def valid_category?(category)
+    return false unless category
+    categories.include?(category)
+  end
+
+  def valid_subcategory?(category, subcategory)
+    return false unless category && subcategory
+    sub_categories_for(category).try(:include?, subcategory)
+  end
+
   def sub_categories_for(category)
-    load_categories[category]
+    load_categories[category] if valid_category?(category)
   end
 
   private
