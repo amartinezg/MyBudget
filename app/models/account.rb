8e4152e334cce186bcaa51ecad8eba6334be44cd
Andres Martinez <amartinez0000@gmail.com>
2016-05-31 19:32:49 -0300

    Import movements from xml process improved

--- app/models/account.rb
+++ app/models/account.rb
@@ -20,7 +20,7 @@ class Account < ActiveRecord::Base
   validates_inclusion_of :type, in: %w(savings credit cash inversion)
   validates_uniqueness_of :name, scope: [:type, :balance_currency], message: "Account already exists"
 
-  XML_ATTRS = {date: "FECHA", category: "CATEGORIA", sub_category: "SUB_CATEGORIA", notes: "DESCRIPCIÓN"}
+  XML_ATTRS = {date: "FECHA", category: "CATEGORIA", sub_category: "SUB_CATEGORIA", notes: "DESCRIPCION"}
 
   def increment_balance(amount)
     increment!(:balance_cents, amount.cents)
@@ -60,7 +60,8 @@ class Account < ActiveRecord::Base
     attributes = {amount_currency: balance_currency, amount: amount, account: self}
     candidates = {
       candidate_expense: Expense.new(attributes),
-      candidate_income: Income.new(attributes)
+      candidate_income: Income.new(attributes),
+      candidate_error: nil
     }
     candidates.find {|method, _| self.send method, amount}.last
   end
@@ -71,6 +72,10 @@ class Account < ActiveRecord::Base
 
   def candidate_income(amount)
     is_credit? && amount < 0 || !is_credit? && amount > 0
+  end
+
+  def candidate_error(amount)
+    amount.nil? || amount == 0
   end
 
 end
